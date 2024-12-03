b109e8702ac247da2cc4e7c1d53f03d3595e3e1f
EdmoCunha <cunha.edmo@rede.ulbra.br>
2024-12-03 20:25:44 -0300

    Subindo Primeiro o código testado do pc

---
+++ Repositories/IPedidoRepository.cs
@@ -0,0 +1,7 @@
+public interface IPedidoRepository{
+    Task<IEnumerable<Pedido>> GetAllAsync();
+    Task<Pedido?> GetByIdAsync(int id);
+    Task AddAsync(Pedido pedido);
+    Task UpdateAsync(Pedido pedido);
+    Task DeleteAsync(int id);
+}
