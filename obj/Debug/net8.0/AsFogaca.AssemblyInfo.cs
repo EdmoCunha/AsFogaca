9bcdf9b38c0a6fea08714320219ae40732bf16eb
EdmoCunha <cunha.edmo@rede.ulbra.br>
2024-12-03 20:37:44 -0300

    Adicionado texto em word

--- obj/Debug/net8.0/AsFogaca.AssemblyInfo.cs
+++ obj/Debug/net8.0/AsFogaca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AsFogaca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b109e8702ac247da2cc4e7c1d53f03d3595e3e1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AsFogaca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AsFogaca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
